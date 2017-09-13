037b03c79117e0ad56650c6cf6f418e2c4824f43
gainfire <gainfire@gmail.com>
2017-09-13 11:08:07 +0530

    rnij

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
+
+//changing the text of the main-text element
 var element = document.getElementById('main-text');
 element.innerHTML = ('Lololol');
+
+//moving the picture with id madi
+var img = document.getElementById('img');
+img.onclick = function() {
+  img.style.marginLeft = '100px';
+};
