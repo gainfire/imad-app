b4529bd0849a6beab8b7130be50bcb2dab55c8ab
gainfire <gainfire@gmail.com>
2017-09-13 10:54:28 +0530

    behf

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text')
-element.innerHTML = ('Lololol')
+var element = document.getElementById('main-text');
+element.innerHTML = ('Lololol');
